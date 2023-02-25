4eaf7c503e2f80193b844a28fb951a61a8a24ad2
HardeepNain <hnspnain@gmail.com>
2023-02-25 23:32:11 +0530

    Pivot point - v0 Org added

--- util.js
+++ util.js
@@ -1,8 +1,9 @@
-  
 module.exports = {
-    "Documents" : ["doc" , "docx" , "pdf" , "txt" , "htm" , "html" , "ppt" , "pptx" , "odt" , "xls" , "xlsx"],
-    "Images" : ["img" , "jpg" , "gif", "png" , "jpeg" , "webp" , "tiff" , "tif" , "eps" , "bmp" , "ppm" , "pgm" , "pbm" , "pnm" , "psd" , "raw" , "heif" , "indd" , "svg"],
-    "Audio" : ["mp3" , "m4a" , "flac" , "wav" , "wma" , "aac" , "ogg" , "dsd" , "alac" , "3ga" , "3gpp"] ,
-    "Video" : ["mp4" , "mkv" , "mov" , "webm" , "mpg" , "mp2" , "mpeg" , "mpe" , "mpv" , "m4p" , "m4v" , "avi" , "wmv" , "qt" , "flv" , "swf" , "avchd"],
+    "Documents" : ["doc" , "pdf" , "txt" ],
+    "Images": ["img" , "jpg" , "gif" , "png"],
+    "Audio": ["mp3"] ,
+    "Video": ["mp4" , "mkv"],
     "Applications" : ["exe"]
 }
+
+
